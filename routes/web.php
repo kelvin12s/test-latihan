680a831fe74a21c725e87fabe29255cdd5eb9c95
kelvin12s <kelvin@itbss.ac.id>
2026-04-27 12:19:43 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+Route::get('/mahasiswa-create', [MahasiswaController::class, 'create'])->name('mahasiswa.add');
+Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.save');
+Route::get('/mahasiswa-edit/{id}', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.delete');
